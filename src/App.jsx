d4badcaff2aed926b8bbea4bfe96caf0afe9f05a
Harsh202002 <harshit@netfotech.in>
2026-02-12 14:44:35 +0530

    chnges added in app

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ import Report from './Candidate/Pages/Report';
 import AppliedJD from './Candidate/Pages/AppliedJD';
 import Examination from './Candidate/Pages/Examination';
 import StartExam from './Candidate/Pages/StartExam';
+import { useState } from "react";
 import CandidateProfile from './Candidate/Pages/CandidateProfile';
 import RMGLayout from './RMGAdmin/Pages/RMGLayout';
 import Requirement from './RMGAdmin/Pages/Requirement';
@@ -71,8 +72,15 @@ import { CompanyProvider } from './Context/companyContext';
 import CandidateRegister from './Candidate/CandidateRegister';
 import CandidateForgotPassword from './components/CandidateForgotPassword';
 import RecruiterProfile from './RecruiterAdmin/RecruiterProfile';
+import LoadingScreen from '../src/components/MainLoader.jsx';
 
 const App = () => {
+
+   const [isLoading, setIsLoading] = useState(true);
+
+  if (isLoading){
+    return<LoadingScreen onLoadingComplete={()=>setIsLoading(false)}/>
+  }
   return (
 
     <CompanyProvider>
